6b510fedbb94ee314ad98a2859cdfcf5ea613486
Kiran <kiranpal1719@gmail.com>
2026-02-25 20:11:26 +0530

    Insert customer data into bank_customer table

---
+++ day2 Insertion data.sql
@@ -0,0 +1,17 @@
+USE BANK;
+INSERT INTO bank_customer (customer_id,name,age,gender,city,mobile,email,account_type,balance) VALUES
+('001','Abhishek','23','Male','Noida','9967357289','abhi@gmail.com','saving',2000),
+('002','Rahul','21','Male','Luckhnow','892645329','rahul@gmail.com','saving',1000),
+('003','Shrisht','28','Female','Ghaziabad','8936494569','shrisht@gmail.com','saving',500),
+('004','Akansha','20','Female','Greater Noida','7839364789','akansha@gmail.com','Current',20000),
+('005','Abhay','19','Male','Delhi','8967458769','abhay@gmail.com','saving',4000),
+('006','Rohan','26','Male','Ghaziabad','8930735288','rohan@gmail.com','saving',1500),
+('007','Ayush','23','Male','Ghaziabad','8846348738','ayush@gmail.com','saving',900),
+('008','Shreya','22','Female','Delhi','9967357289','shreya@gmail.com','Current',20000),
+('009','Shristi','25','Female','Greater Noida','9033735899','shristi@gmail.com','Current',20000),
+('010','Suhana','22','Female','Delhi','7849634588','suhana@gmail.com','Current',22000),
+('011','Surbhi','21','Female','Greater Noida','9974638599','surbhi@gmail.com','saving',4500),
+('012','Rohit','22','Male','Ghaziabad','8899736375','rohit@gmail.com','saving',900),
+('013','Aakash','21','Male','Luckhnow','8976475698','aakash@gmail.com','saving',9500);
+
+SELECT * FROM bank_customer;
